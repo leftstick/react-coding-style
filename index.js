00bd37ea8ca64e09ff45b33ecab4f5490bbc6ded
Howard.Zuo <leftstick@qq.com>
2020-09-15 11:20:33 +0800

    feat: add ts lint

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const eslint = require('./src/eslint')
 const prettier = require('./src/prettier')
 
-module.exports.eslint = eslint
+module.exports.eslint = eslint('js')
+module.exports.tslint = eslint('ts')
 module.exports.prettier = prettier
--- src/eslint.js
+++ src/eslint.js
@@ -1,15 +1,18 @@
 const reactAppConfig = require('eslint-config-react-app')
 
-module.exports = {
-  ...reactAppConfig,
-  rules: {
-    ...reactAppConfig.rules,
-    strict: [0],
-    'no-sequences': [0],
-    'no-mixed-operators': [0],
-    'react/react-in-jsx-scope': [0],
-    'no-useless-escape': [0],
-    'max-len': ['error', { code: 120 }],
-    'react/prop-types': ['error']
+module.exports = function (type) {
+  return {
+    ...reactAppConfig,
+    rules: {
+      ...reactAppConfig.rules,
+      strict: [0],
+      'no-sequences': [0],
+      'no-mixed-operators': [0],
+      'react/react-in-jsx-scope': [0],
+      'no-useless-escape': [0],
+      'max-len': ['error', { code: 120 }],
+      'react/prop-types': [type === 'js' ? 'error' : 'off']
+    }
   }
+  
 }
